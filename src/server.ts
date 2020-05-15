76ba8ea297c506498dba4ef98b218f0118958e0a
Thiago Brito <britotr@gmail.com>
2020-05-15 14:42:08 -0300

    Finished configuration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
+app.use(routes);
 
-app.listen(3333)
+app.listen(3333);
