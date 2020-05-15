96707721ae1215972cf5a9d6e4ce98305c4c40ea
Thiago Brito <britotr@gmail.com>
2020-05-15 15:51:14 -0300

    add try catch in search about list

--- src/routes/about.routes.ts
+++ src/routes/about.routes.ts
@@ -6,8 +6,12 @@ const routes = Router();
 
 routes.get('/', async (request, response) => {
   const aboutRepository = getCustomRepository(AboutRepository);
-  const aboutList = await aboutRepository.find();
-  return response.json(aboutList);
+  try {
+    const aboutList = await aboutRepository.find();
+    return response.json(aboutList);
+  } catch (err) {
+    return response.status(401).json(err);
+  }
 });
 
 export default routes;
